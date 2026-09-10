dea5ad0334fd86a65588963adb35eac1e8693086
Muthuraj <rajbala2816@gmail.com>
2026-09-10 16:24:57 +0530

    Fix automated test

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ def test_home_page():
     client = app.test_client()
     response = client.get("/")
 
-    assert response.status_code == 500
+    assert response.status_code == 200
 
 
 def test_health_check():
